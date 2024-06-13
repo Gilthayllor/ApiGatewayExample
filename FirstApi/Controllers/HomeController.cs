1fc1b585f13d931f0aaffbf356171a03f07fed43
Gilthayllor Brandão de Sousa <gilthayllor@outlook.com>
2024-06-13 14:42:39 -0300

    Criação das duas APIs

---
+++ FirstApi/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet("[action]")]
+        public IActionResult GetName()
+        {
+            return Ok("Gilthayllor Sousa");
+        }
+    }
+}
