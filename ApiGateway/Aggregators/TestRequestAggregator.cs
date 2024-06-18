432ca9ecab84f3e6c7a88ce1709b7255a8121874
Gilthayllor Brandão de Sousa <gilthayllor@outlook.com>
2024-06-18 12:06:53 -0300

    Adição de log no aggregator

--- ApiGateway/Aggregators/TestRequestAggregator.cs
+++ ApiGateway/Aggregators/TestRequestAggregator.cs
@@ -7,8 +7,17 @@ namespace ApiGateway.Aggregators
 {
     public class TestRequestAggregator : IDefinedAggregator
     {
+        private readonly ILogger _logger;
+
+        public TestRequestAggregator(ILogger logger)
+        {
+            _logger = logger;
+        }
+
         public async Task<DownstreamResponse> Aggregate(List<HttpContext> responses)
         {
+            _logger.LogInformation("Calling {AggregatorName}", nameof(TestRequestAggregator));
+
             var results = responses.Select(x => x.Items.DownstreamResponse()).ToList();
 
             var contentList = new List<string>();
